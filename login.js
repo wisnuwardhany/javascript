4e319861453e2a0ed7076e3791850b5254f154a6
wisnuwardhany <wisnuwardhany@gmail.com>
2016-08-24 23:00:38 +0700

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-document.write('var password;
-var pass=&quot;wisnucyber&quot;;
-password=prompt(&#39;Enter your password - Copyright 2016&#39;,&#39; &#39;);
-if (password==pass) alert(&#39;Correct password, OK to enter!&#39;);
-else {
-  window.location=&quot;http://wisnu.datappilncirebon.tk/&quot;;
-}');;
