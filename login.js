fa442102da8815cafe036d35d3395486416050d0
wisnuwardhany <wisnuwardhany@gmail.com>
2016-08-24 22:56:36 +0700

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,7 @@
-<script language='JavaScript'>
-var password;
+document.write('var password;
 var pass=&quot;wisnucyber&quot;;
 password=prompt(&#39;Enter your password - Copyright 2016&#39;,&#39; &#39;);
 if (password==pass) alert(&#39;Correct password, OK to enter!&#39;);
 else {
   window.location=&quot;http://wisnu.datappilncirebon.tk/&quot;;
-}
-</script>
+}'))
