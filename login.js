2ed2db916d625203e85979e9673efc32a466e0d3
wisnuwardhany <wisnuwardhany@gmail.com>
2016-08-24 22:58:58 +0700

    Update login.js

--- login.js
+++ login.js
@@ -4,4 +4,4 @@ password=prompt(&#39;Enter your password - Copyright 2016&#39;,&#39; &#39;);
 if (password==pass) alert(&#39;Correct password, OK to enter!&#39;);
 else {
   window.location=&quot;http://wisnu.datappilncirebon.tk/&quot;;
-}'))
+}');;
