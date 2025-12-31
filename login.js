b46c117bdc3472aca651c4c30ffdcbcdd54ff347
xodhox <wisnuwardhany@gmail.com>
2025-12-31 18:15:09 +0700

    Delete login.js

--- login.js
+++
@@ -1,8 +0,0 @@
-document.write('var password;
-var pass=&quot;wisnucyber&quot;;
-password=prompt(&#39;Enter your password - Copyright 2016&#39;,&#39; &#39;);
-if (password==pass) alert(&#39;Correct password, OK to enter!&#39;);
-else {
-  window.location=&quot;http://yourdomain.com/&quot;;
-
-}
